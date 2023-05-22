bbf7b771a4b83ab24e3a715438e4f2c9dc5e8e01
nrismawardah <nrismawardah@gmail.com>
2023-05-22 11:36:43 +0700

    Membuat kelas pelajar

--- Pewarisan/Pewarisan.cpp
+++ Pewarisan/Pewarisan.cpp
@@ -17,4 +17,19 @@ public:
 	int jumlah(int a, int b) {
 		return a + b;
 	}
+
+};
+
+class pelajar : public orang {
+public:
+	string sekolah;
+
+	pelajar(string pNama, string pSekolah) :
+		orang(pNama),
+		sekolah(pSekolah) {
+		cout << "pelajar dibuat\n" << endl;
+	}
+	~pelajar() {
+		cout << "pelajar dihapus\n" << endl;
+	}
 };
