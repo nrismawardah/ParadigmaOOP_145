774ed878adf47646b67ce851946482e2b2024167
nrismawardah <nrismawardah@gmail.com>
2023-05-22 15:13:51 +0700

    Membuat class pasien

--- Asosiasi/Asosiasi.cpp
+++ Asosiasi/Asosiasi.cpp
@@ -3,3 +3,14 @@
 using namespace std;
 
 class dokter;
+class pasien {
+public:
+	string nama;
+	vector<dokter*> daftar_dokter;
+	pasien(string pNama) :nama(pNama) {
+		cout << "Pasien \"" << nama << "\" ada \n";
+	}
+	~pasien() {
+		cout << "Pasien \"" << nama << "\" tidak ada\n";
+	}
+};
